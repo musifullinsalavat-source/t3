286ab51b421b3447ad10f669ac730ce2ee2d45f2
Faisal Albasu <albasfaisal@yandex.ru>
2024-10-17 14:12:24 +0500

    switched to streamlit

---
+++ t3/app.py
@@ -0,0 +1,29 @@
+from pandas.core.frame import StringIO
+import streamlit as st
+from pathlib import Path
+
+# def upload_file(file):
+#     filename = Path(file).name
+#     if file is not None:
+#         with open(filename) as f:
+#             text = f.read()
+            
+#         return text
+
+with st.sidebar:
+    st.button("login")
+        
+file = st.file_uploader("Upload file")
+if file is not None:
+    filename = Path(file.name).name
+    bytes_data = file.getvalue()
+    stringio = StringIO(file.getvalue().decode("utf-8"))
+    string_data = stringio.read()
+    text = file.read().decode("utf-8")
+    st.write(filename)
+    st.text_area("Output", value = text, disabled=True)
+    st.download_button("Download file", data = text, file_name=filename)
+    st.feedback()
+    # st.write("Reading file line by line")
+    # for line in text.splitlines():
+    #     st.write(line)
--- t3/ui.py
+++ t3/ui.py
@@ -1,24 +1,56 @@
-import gradio as gr
+# import gradio as gr
+# from pathlib import Path
 
-def upload_file(file):
-    if file is not None:
-        with open(file.name) as f:
-            text = f.read()
+# def login():
+#     return gr.LoginButton()
+
+# def upload_file(file):
+#     if file is not None:
+#         with open(file.name) as f:
+#             text = f.read()
             
-        return text, gr.DownloadButton(label = "Download File", value = file)
+#         return text, Path(file.name).name
+        
+# def download_file(text, filename):
+#     return text, filename
+        
+# def generate_tests():
+#     """
+#     Takes in lecture(s) in text format as input, feeds them to the LLM
+#     which then produces the tests as ouput based on the lecture(s) and
+#     the number of tests specified by user
+#     """
+#     pass
+    
+# def save_inputs():
+#     """
+#     Saves inputs (lecture files and number of tests needed to the db and storage)
+#     """
+#     pass
 
-# with gr.Blocks(title="T3") as demo:
-#     with gr.Row():
-#         with gr.Column(scale=1):
-#             file_dropzone = gr.File(label="Upload Lecture")
-#             # upload_btn = gr.UploadButton(label="Choose Lecture File")
-#         with gr.Column(scale=2):
-#             output = gr.Textbox(label="Output Box")
-# 
-demo = gr.Interface(
-    fn = upload_file,
-    inputs = ["files"],
-    outputs = ["textbox", "downloadbutton"]
-)
+# def main(file):
+#     text = upload_file(file)
+    
 
-demo.launch()
+# # with gr.Blocks(title="T3") as demo:
+# #     login_button = gr.LoginButton
+# #     with gr.Row():
+# #         with gr.Column(scale=1):
+# #             file_dropzone = gr.File(label="Upload Lecture")
+# #         with gr.Column(scale=2):
+# #             output = gr.Textbox(label="Output Box")
+# #             download_button = gr.DownloadButton(label = "Download file")
+            
+# #     file_dropzone.change(upload_file, inputs = file_dropzone, outputs = [output, download_button])
+    
+# #     # output.change(download_file, inputs = [output, file_dropzone], outputs = [download_button])
+# #     output.change(lambda text: (text, Path(file_dropzone).name) if text else (None, None),
+# #         inputs = output, outputs = [download_button])
+# # 
+# demo = gr.Interface(
+#     fn = upload_file,
+#     inputs = ["loginbutton", "files", gr.Slider(5, 50, value = 5, step = 5, interactive = True)],
+#     outputs = ["textbox", "downloadbutton", "loginbutton"]
+# )
+
+# demo.launch(auth = ("username", "password"))
